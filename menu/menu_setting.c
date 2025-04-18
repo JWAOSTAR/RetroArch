9484cd605fa889cb41afb8c0ead47ebe8c528de9
JWAOSTAR <jwaostar@gmail.com>
2025-04-18 16:48:15 +0300

    rebuild

--- menu/menu_setting.c
+++ menu/menu_setting.c
@@ -4668,6 +4668,10 @@ static size_t setting_get_string_representation_uint_ozone_menu_color_theme(
             return strlcpy(s,
                   msg_hash_to_str(
                      MENU_ENUM_LABEL_VALUE_OZONE_COLOR_THEME_SOLARIZED_LIGHT), len);
+         case OZONE_COLOR_THEME_RED_DARK:
+            return strlcpy(s,
+                  msg_hash_to_str(
+                     MENU_ENUM_LABEL_VALUE_OZONE_COLOR_THEME_RED_DARK), len);
          case OZONE_COLOR_THEME_GRAY_DARK:
             return strlcpy(s,
                   msg_hash_to_str(
